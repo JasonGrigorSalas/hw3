0fd14db9054fa31cfef407c899ec102aee34a26e
JasonGrigorSalas <jasoncoco1@gmail.com>
2022-04-07 12:43:36 -0400

    Edited a few files.

--- components/quizTally.py
+++ components/quizTally.py
@@ -4,18 +4,18 @@ def total(value):
         vars.character = vars.character [0]
         print("It's " + vars.character)
 
-    if value <= 1:
-        vars.character = vars.character [2]
-        print("It's " + vars.character)
-
-    if value <= 2:
-        vars.character = vars.character [4]
-        print("It's " + vars.character)
-
     if value <= 3:
         vars.character = vars.character [1]
         print("It's " + vars.character)
 
+    if value <= 1:
+        vars.character = vars.character [2]
+        print("It's " + vars.character)
+
     if value <= 0:
         vars.character = vars.character [3]
+        print("It's " + vars.character)
+
+    if value <= 2:
+        vars.character = vars.character [4]
         print("It's " + vars.character)
