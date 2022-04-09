9a5619ae20d2b1b73e1c48580f0c8f3706c84adf
JasonGrigorSalas <jasoncoco1@gmail.com>
2022-04-09 16:15:42 -0400

    Finally, it works!

--- components/quizTally.py
+++ components/quizTally.py
@@ -1,33 +1,33 @@
-from .vars import vars
+from .vars import quizTotal, character, characters, images
 from PIL import Image
 
 def total(value):
-    if value <= 4:
-        vars.character = vars.character [0]
-        print("It's " + vars.character)
-        zeusImage = Image.open("zeus.jpg")
+    if value == 4:
+        character = characters [0]
+        print("It's " + character)
+        zeusImage = Image.open("components/images/zeus.jpg")
         zeusImage.show()
 
-    if value <= 3:
-        vars.character = vars.character [1]
-        print("It's " + vars.character)
-        athenaImage = Image.open("athena.jpg")
+    if value == 3:
+        character = characters [1]
+        print("It's " + character)
+        athenaImage = Image.open("components/images/athena.jpg")
         athenaImage.show()
 
-    if value <= 1:
-        vars.character = vars.character [2]
-        print("It's " + vars.character)
-        poseidonImage = Image.open("poseidon.jpg")
+    if value == 1:
+        character = characters [2]
+        print("It's " + character)
+        poseidonImage = Image.open("components/images/poseidon.jpg")
         poseidonImage.show()
 
-    if value <= 0:
-        vars.character = vars.character [3]
-        print("It's " + vars.character)
-        hadesImage = Image.open("poseidon.jpg")
+    if value == 0:
+        character = characters [3]
+        print("It's " + character)
+        hadesImage = Image.open("components/images/hades.jpg")
         hadesImage.show()
 
-    if value <= 2:
-        vars.character = vars.character [4]
-        print("It's " + vars.character)
-        gaiaImage = Image.open("gaia.jpg")
+    if value == 2:
+        character = characters [4]
+        print("It's " + character)
+        gaiaImage = Image.open("components/images/gaia.jpg")
         gaiaImage.show()
